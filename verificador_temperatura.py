c2c65b3fe879e7ff59b859d2e124e8960ec3f551
Victor Santillan <santillanvi@globalhits.com>
2025-08-16 16:40:21 -0600

    temp

---
+++ verificador_temperatura.py
@@ -0,0 +1,8 @@
+temp = float(input("Ingresa una tempreratura"))
+
+if temp < 10:
+    print("El día de hoy esta frío")
+elif temp >=10 && temp <=25:
+    print("El día de hoy esta calido")
+else:
+    print("Hoy esta muy caliente")
