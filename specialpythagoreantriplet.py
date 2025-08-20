e32681b32c4f8caa5dee09666ef932f7c0d6f507
Victor Santillan <santillanvi@globalhits.com>
2025-08-19 22:38:32 -0600

    Problem 9: Special Pythagorean Triplet

---
+++ specialpythagoreantriplet.py
@@ -0,0 +1,16 @@
+## Plataforma: Project Euler 
+## Problem 9: Special Pythagorean Triplet
+## Fecha: 19/08/2025
+
+###### Problem
+## A Pythagorean triplet is a set of three natural numbers, $a \lt b \lt c$, for which,
+## a^2 + b^2 = c^2
+## For example, $3^2 + 4^2 = 9 + 16 = 25 = 5^2$.
+## There exists exactly one Pythagorean triplet for which a + b + c = 1000 Find the product abc
+## -------------------------
+
+
+
+
+
+
