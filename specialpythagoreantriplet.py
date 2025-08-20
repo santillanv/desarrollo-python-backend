95b09022bbce0a37d50459036565dfa9c0b04f53
Victor Santillan <santillanvi@globalhits.com>
2025-08-20 11:35:01 -0600

    Problem 9: Special Pythagorean Triplet v2

--- specialpythagoreantriplet.py
+++ specialpythagoreantriplet.py
@@ -9,8 +9,16 @@
 ## There exists exactly one Pythagorean triplet for which a + b + c = 1000 Find the product abc
 ## -------------------------
 
+def special_triplet_sum_1000():
+    target = 1000
+    # a < b < c  ⇒  a como mucho 332 y b < (1000 - a)/2
+    for a in range(1, target // 3):                     # a < 333
+        for b in range(a + 1, (target - a) // 2):       # b < c ⇒ b < (1000-a)/2
+            c = target - a - b                          # evita el tercer bucle
+            if a*a + b*b == c*c:
+                print("Triplete:", a, b, c)
+                print("Producto:", a*b*c)
+                return a, b, c, a*b*c                   # SALE inmediatamente
+    return None
 
-
-
-
-
+print(special_triplet_sum_1000())
